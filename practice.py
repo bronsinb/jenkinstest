85294a73ac9f8afbd352bf95ed7c187b712d260e
bronsinb <benyaminbronsin@gmail.com>
2022-03-26 14:54:44 -0700

    New Test

--- practice.py
+++ practice.py
@@ -1,5 +1,7 @@
 import pytest
 import unittest
+import time
+from selenium import webdriver
 
 
 class Math:
@@ -8,12 +10,23 @@ class Math:
         return x + y
 
 
+browser = webdriver.Chrome('/Users/bronsinb/Downloads/chromedriver')
 class Test(unittest.TestCase):
 
-    @pytest.mark.add
     def test_add(self):
-        assert Math.add(1, 2) == 3, "Incorrect adding" 
+        assert Math.add(1, 2) == 3
 
-    @pytest.mark.addtwice
-    def test_add_twice(self):
-        assert Math.add(2, Math.add(1, 2)) == 5, "Not Correct"
+    def test_costco(self):
+        print("Step 1: Set Up Browser")
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome('/Users/bronsinb/Downloads/chromedriver', options=options)
+
+        print("Step 2: Go to Costco")
+        assert "signin" in "https://signin.costco.com", "Costco.com did not load"
+
+        print("Step 3: Log In")
+        print("Input credentials")
+        assert "Auto Mation" in "Welcome Auto Mation!", "Sign in failed"
+
+        print("Step 4: Finalize")
+        driver.close()
